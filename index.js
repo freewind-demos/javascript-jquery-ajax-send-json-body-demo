48993da4197fb496d920ae2c9bd3a09959712db5
freewind <nowindlee@gmail.com>
2018-09-23 12:00:08 +0800

    demo

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var main = document.getElementById("main");
-main.innerHTML = "Hello, JS!";
-
+$('#jquery-ajax-trigger').click(() => {
+    $.ajax({
+        url: '/data.json',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            username: 'freewind'
+        }),
+        success: (data) => {
+            $('#data').text(JSON.stringify(data, null, 2))
+        }
+    })
+})
