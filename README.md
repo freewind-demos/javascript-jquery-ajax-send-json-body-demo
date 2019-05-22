ba7827c6b18b51d68e66d9d8a5e8d1dca204de34
freewind <nowindlee@gmail.com>
2019-05-22 16:58:01 +0800

    improve

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 JavaScript JQuery Ajax Send Json Body Demo
 ==========================================
 
+注意：在jquery中，要发送json body，反而需要先把数据使用`JSON.stringify`变成字符串，
+同时再加上`contentType: 'application/json'`才行。。。
+
 ```
 npm install
 npm run demo
 ```
-
-Then open <http://localhost:3000>
-
-![demo](./images/demo.jpg)
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $('#jquery-ajax-trigger').click(() => {
-    $.ajax({
-        url: '/data.json',
-        method: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            username: 'freewind'
-        }),
-        success: (data) => {
-            $('#data').text(JSON.stringify(data, null, 2))
-        }
-    })
+  $.ajax({
+    url: 'http://httpbin.org/post',
+    method: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      username: 'freewind'
+    }),
+    success: (data) => {
+      $('#data').text(JSON.stringify(data, null, 2))
+    }
+  })
 })
